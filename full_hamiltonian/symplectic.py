29930cfdeeb4789b0dea183b8c9a36aa3be17cbd
Eduardo Villasenor <evalvarez12@gmail.com>
2019-04-23 16:31:43 +1000

    Single mode comparisson working ?

--- full_hamiltonian/symplectic.py
+++ full_hamiltonian/symplectic.py
@@ -14,8 +14,11 @@ import scipy.linalg as la
 
 def H_single_mode_squeeze(z):
     H = np.zeros([4, 4], dtype=complex)
-    H[2, 0] = 1j * z.conjugate()/2
-    H[0, 2] = -1j * z/2
+    H[2, 0] = 1j * z.conjugate()
+    H[0, 2] = -1j * z
+    
+#    H[2, 0] = z.conjugate()
+#    H[0, 2] = z
     return H
 
 
@@ -60,9 +63,9 @@ def Symplectic(H_mat):
 #    size = H_mat.shape[0]
     L = quad_basis_transform()
     T = quad_basis_reorder()
-#    S = np.dot(L.transpose().conjugate(), np.dot(S, L))
-    return S
-#    return np.dot(T, np.dot(S, T))
+    S = np.dot(L.transpose().conjugate(), np.dot(S, L))
+#    return S
+    return np.dot(T, np.dot(S, T))
 
 
 def quad_basis_transform():
@@ -78,40 +81,23 @@ def quad_basis_reorder():
     return T
 
 ###################################################################
-N = 2
-z = 2
+N = 3
+
+r = 2
+phase = np.pi/2*0
+z = r * np.exp(1j*phase)
+z = r
 
 a = qt.tensor(qt.destroy(N), qt.qeye(N))
 b = qt.tensor(qt.qeye(N), qt.destroy(N))
 
-H_mat = H_two_mode_squeeze(z)
-H = Hamiltonian(N, H_mat)
-
-H_ref = 1j*z.conjugate()*a*b - 1j*z*a.dag()*b.dag()
-U_ref = (-1j*H_ref).expm()
-
-U_qtref = qt.squeezing(a, b, 2*z)
-print(H == H_ref)
-print(U_ref == U_qtref)
-
-S = Symplectic(H_mat)
-print(S)
-
-print('Ref ------')
-ch = np.cosh(z)
-sh = np.sinh(z)
-S_ref =np.array([[ch, 0, sh, 0], [0, ch, 0, -sh], [sh, 0, ch, 0], [0, -sh, 0, ch]])
-print(S_ref)
-
-print('-----------------------------------')
-#
-#H_mat = H_single_mode_squeeze(z)
+#H_mat = H_two_mode_squeeze(z)
 #H = Hamiltonian(N, H_mat)
 #
-#H_ref = (1j*z.conjugate()*a**2 - 1j*z*a.dag()**2)/2
+#H_ref = 1j*z.conjugate()*a*b - 1j*z*a.dag()*b.dag()
 #U_ref = (-1j*H_ref).expm()
 #
-#U_qtref = qt.squeezing(a, a, z)
+#U_qtref = qt.squeezing(a, b, 2*z)
 #print(H == H_ref)
 #print(U_ref == U_qtref)
 #
@@ -119,9 +105,32 @@ print('-----------------------------------')
 #print(S)
 #
 #print('Ref ------')
-#ch = np.exp(z)
-#sh = np.exp(-z)
-#S_ref =np.array([[ch, 0, 0, 0], [0, sh, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+#ch = np.cosh(z)
+#sh = np.sinh(z)
+#S_ref =np.array([[ch, 0, sh, 0], [0, ch, 0, -sh], [sh, 0, ch, 0], [0, -sh, 0, ch]])
 #print(S_ref)
+
+print('-----------------------------------')
+
+H_mat = H_single_mode_squeeze(z)
+H = Hamiltonian(N, H_mat)
+
+H_ref = (1j*z.conjugate()*a**2 - 1j*z*a.dag()**2)
+
+U_qtref = qt.squeezing(a, a, z)
+print(H == H_ref)
+print(U_ref == U_qtref)
+
+S = Symplectic(H_mat)
+print(S)
+
+print('Ref ------')
+phasee = np.pi/2*0
+cc = np.cos(phasee)
+ss = np.sin(phasee)
+ch = np.cosh(r)
+sh = np.sinh(r)
+S_ref =np.array([[ch + cc*sh, ss*sh, 0, 0], [ss*sh, ch- cc*sh, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+print(S_ref)
 
 
