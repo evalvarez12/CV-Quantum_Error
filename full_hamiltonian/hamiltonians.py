Fixed factor of 2 error in getting the symplectic representation

full_hamiltonian/hamiltonians.py
@@ -29,38 +29,35 @@ def two_mode_squeeze(N, z):
 
 def H_single_mode_squeeze(z):
     H = np.zeros([4, 4], dtype=complex)
-    H[2, 0] = (1j * z.conjugate())/2
-    H[0, 2] = (-1j * z)/2
-
-#    H[2, 0] = z.conjugate()
-#    H[0, 2] = z
+    H[2, 0] = (-1j * z.conjugate())/2
+    H[0, 2] = (1j * z)/2
     return H
 
 
 def H_two_mode_squeeze(z):
     H = np.zeros([4, 4], dtype=complex)
-    H[3, 0] = (1j * z.conjugate())/2
-    H[0, 3] = (-1j * z)/2
+    H[3, 0] = (-1j * z.conjugate())/2
+    H[0, 3] = (1j * z)/2
 
-    H[2, 1] = (1j * z.conjugate())/2
-    H[1, 2] = (-1j * z)/2
+    H[2, 1] = (-1j * z.conjugate())/2
+    H[1, 2] = (1j * z)/2
     return H
 
 
 def H_beam_splitter(z):
     H = np.zeros([4, 4], dtype=complex)
-    H[1, 0] = -1j * z.conjugate()
-    H[0, 1] = 1j * z
+    H[1, 0] = (-1j * z.conjugate())/2
+    H[0, 1] = (1j * z)/2
 
-    H[3, 2] = 1j*z
-    H[2, 3] = -1j*z.conjugate()
+    H[3, 2] = (1j * z)/2
+    H[2, 3] = (-1j*z.conjugate())/2
     return H
 
 
 def H_phase_shift(theta):
     H = np.zeros([4, 4], dtype=complex)
-    H[0, 0] = -theta
-    H[2, 2] = -theta
+    H[0, 0] = -theta/2
+    H[2, 2] = -theta/2
     return H
 
 

full_hamiltonian/symplectic.py
@@ -26,7 +26,8 @@ def phase_shift(z):
 
 def single_mode_squeeze(z):
     H = ham.H_single_mode_squeeze(z)
-    S = symplectic(H)
+    # Extra factor of 2 requied specifically for this case
+    S = symplectic(2*H)
     return S
 
 
@@ -38,7 +39,8 @@ def two_mode_squeeze(z):
 
 def symplectic(H_mat):
     K = np.block([[np.eye(2), np.zeros([2, 2])], [np.zeros([2, 2]), -np.eye(2)]])
-    S = la.expm(-1j*np.dot(K, H_mat))
+    # Extra factor on the Hamiltonian matrix requied to get correct representation
+    S = la.expm(-1j*np.dot(K, 2 * H_mat))
 
 #    size = H_mat.shape[0]
     L = quad_basis_transform()

full_hamiltonian/test_cv_system2.py
@@ -23,23 +23,30 @@ t = np.pi/3
 wx = np.linspace(-5, 5)
 
 
+# Testing two mode squeeze
+
 r = np.arcsinh(np.sqrt(mpn))
 print("Squeezing:", r)
 
 ## Initialize state
 sys = cv.System(N, Nmodes=2, cm=True)
-print(sys.cm)
+print("Initial CM:", sys.cm)
 
 
 sys.apply_TMS(mpn, [0, 1])
-S_theory = theory.two_mode_squeeze(r)
-print("Theory S:", S_theory)
 
 
-print(sys.cm)
+
+print("Sys CM:", sys.cm)
 
 
 
 sys.set_quadratures_basis()
 CM = sys.get_full_CM()
 print("CM:", CM)
+
+
+S_theory = theory.two_mode_squeeze(r)
+print("Theory S:", S_theory)
+
+print("Theory CM:", tools.matrix_sandwich(S_theory, np.eye(4)))

full_hamiltonian/test_symplectic.py
@@ -8,16 +8,16 @@ Created on Tue Apr 23 16:48:41 2019
 """
 
 from hamiltonians import *
+from symplectic import *
 import numpy as np
 import qutip as qt
+import theory
 
 N = 3
 
-r = 2
+#r = 2
 # phase = np.pi/2*0
 # z = r * np.exp(1j*phase)
-z = r
-theta = np.pi/6
 
 a = qt.tensor(qt.destroy(N), qt.qeye(N))
 b = qt.tensor(qt.qeye(N), qt.destroy(N))
@@ -25,95 +25,87 @@ b = qt.tensor(qt.qeye(N), qt.destroy(N))
 #############################
 print('-------------- TWO MODE SQUEEZING ---------------------')
 
-
+z = 2 * np.exp(1j*np.pi/4)
 H_mat = H_two_mode_squeeze(z)
 H = Hamiltonian(N, H_mat)
 
-H_ref = 1j*z.conjugate()*a*b - 1j*z*a.dag()*b.dag()
-U_ref = (-1j*H_ref).expm()
 
-U_qtref = qt.squeezing(a, b, 2*z)
+H_ref = -1j * z.conjugate() * a * b  + 1j * z * a.dag() * b.dag()
+U_ref = (-1j * H_ref).expm()
+
+# Qutip needs a -1 and in TMS a factor of 2 in squeezing parameter
+U_qtref = qt.squeezing(a, b, -2*z)
 print("H == H_ref:", H == H_ref)
-print("U == U_ref:", U_ref == U_qtref)
+print("U2 == U_ref:", U_ref == U_qtref)
 
-S = Symplectic(H_mat)
-print(np.round(S, 4))
+S = symplectic(H_mat)
+print(np.round(S, 5))
 
 print('Ref ------')
-ch = np.cosh(-z/2)
-sh = np.sinh(-z/2)
-S_ref =np.array([[ch, 0, sh, 0], [0, ch, 0, -sh], [sh, 0, ch, 0], [0, -sh, 0, ch]])
-print(S_ref)
+S_ref = theory.two_mode_squeeze(z)
+print(np.round(S_ref, 5))
+
 
 print('--------------- SINGLE MODE SQUEEZING --------------------')
 
 H_mat = H_single_mode_squeeze(z)
 H = Hamiltonian(N, H_mat)
 
-H_ref = (1j*z.conjugate()*a**2 - 1j*z*a.dag()**2)/2
-U_ref = (-1j*H_ref).expm()
+H_ref = (-1j * z.conjugate() * a**2 + 1j * z * a.dag()**2)/2
+U_ref = (-1j * H_ref).expm()
 
-U_qtref = qt.squeezing(a, a, z)
+# Qutip need a -1 in squeezing paramenter
+U_qtref = qt.squeezing(a, a, -z)
 print("H == H_ref:", H == H_ref)
 print("U == U_ref:", U_ref == U_qtref)
 
-S = Symplectic(H_mat)
+S = symplectic(H_mat)
 print(np.round(S, 4))
 
 print('Ref ------')
-phasee = np.pi/2*0
-cc = np.cos(phasee)
-ss = np.sin(phasee)
-ch = np.cosh(-r/2)
-sh = np.sinh(-r/2)
-S_ref =np.array([[ch + cc*sh, ss*sh, 0, 0], [ss*sh, ch- cc*sh, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-print(S_ref)
+S_ref = theory.single_mode_squeeze(z)
+print(np.round(S_ref, 5))
+
 
 print('--------------- PHASE SHIFT --------------------')
 
+theta = np.pi/6
 H_mat = H_phase_shift(theta)
 H = Hamiltonian(N, H_mat)
 
-H_ref = -theta*(a * a.dag() + a.dag() * a)
-U_ref = (-1j*H_ref).expm()
+H_ref = -theta*(a * a.dag() + a.dag() * a)/2
+U_ref = (-1j * H_ref).expm()
 
 # U_qtref = qt.phase_gate(N, theta)
 print("H == H_ref:", H == H_ref)
 # print(U_ref == U_qtref)
 
-S = Symplectic(H_mat)
+S = symplectic(H_mat)
 print(np.round(S, 4))
 
 print('Ref ------')
-cc = np.cos(theta)
-ss = np.sin(theta)
-S_ref =np.array([[cc, -ss, 0, 0], [ss, cc, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-print(S_ref)
+S_ref = theory.phase_shift(theta)
+print(np.round(S_ref, 5))
 
 Sps = S_ref
+
+
 print('--------------- BEAM SPLITTER --------------------')
 z = np.pi/4
 
 H_mat = H_beam_splitter(z)
 H = Hamiltonian(N, H_mat)
 
-H_ref = (-1j*z.conjugate()*a * b.dag() + 1j*z* a.dag() * b - 1j*z.conjugate()*b.dag()*a + 1j*z*b*a.dag())
-# H_ref = -theta*(a * b.dag() + b.dag() * a)
-
-U_ref = (-1j*H_ref).expm()
+H_ref = (-1j * z.conjugate() * a * b.dag() + 1j * z * a.dag() * b)
+U_ref = (-1j * H_ref).expm()
 
 # U_qtref = qt.phase_gate(N, theta)
 print("H == H_ref:", H == H_ref)
 # print(U_ref == U_qtref)
 
-S = Symplectic(H_mat)
+S = symplectic(H_mat)
 print(np.round(S, 4))
 
 print('Ref ------')
-c1 = np.cos(np.absolute(z))
-s1 = np.sin(np.absolute(z))
-c2 = np.cos(np.angle(z))
-s2 = np.sin(np.angle(z))
-
-S_ref =np.array([[c1, 0, s1*c2, s1*s2], [0, c1, -s1*s2, s1*c2], [-s1*c2, -s1*s2, c1, 0], [s1*s2, -s1*c2, 0, c1]])
-print(S_ref)
+S_ref = theory.beam_splitter(z)
+print(np.round(S_ref, 5))