116490aaf4229b7a9d0eb8623e7e520d4c732619
Eduardo Villaseñor <evalvarez12@gmail.com>
2019-06-16 19:21:18 +1000

    Added some data

--- plot_PS.py
+++ plot_PS.py
@@ -19,28 +19,28 @@ def plot(N, params, MG=True):
 #    options = ['none', 'tps', 'rps']
 
     for option in options:
-        
+
         if option == 'rsc':
             filename = names.measurements_line(10, 'KR', params, option)
             filename_ind = names.indeces_line(10, 'KR', params, option, 'eta')
         else:
             filename = names.measurements_line(N, 'KR', params, option)
             filename_ind = names.indeces_line(N, 'KR', params, option, 'eta')
-       
-        
+
+
         k_rate = np.load(filename + ".npy")
         key_rates += [k_rate]
-    
+
         inds = np.load(filename_ind + '.npy')
         indeces += [inds]
-    
-    
+
+
     fig1, ax = plt.subplots()
     lines_types = ['k*-', 'b*-', 'r*-', 'y*-', 'm*-', 'c*-', 'g*-']
     for i in range(len(key_rates)):
         ax.plot(indeces[i], key_rates[i], lines_types[i], label=options[i])
-    
-    
+
+
     if MG:
         mg_ratesNO = scipy.io.loadmat('data/rate_no.mat')
         mg_ratesR = scipy.io.loadmat('data/rate_r.mat')
@@ -48,42 +48,110 @@ def plot(N, params, MG=True):
         mg_indNO = scipy.io.loadmat('data/ind_no.mat')
         mg_indR = scipy.io.loadmat('data/ind_r.mat')
         mg_indT = scipy.io.loadmat('data/ind_t.mat')
-        
+
         mg_ratesNO = mg_ratesNO['RateNo'][0]
         mg_ratesR = mg_ratesR['RateR'][0]
         mg_ratesT = mg_ratesT['RateT'][0]
         mg_indNO = mg_indNO['indd'][0]
         mg_indR = mg_indR['indR'][0]
         mg_indT = mg_indT['indT'][0]
-        
+
         ax.plot(mg_indNO, mg_ratesNO, 'k--')
         ax.plot(mg_indR, mg_ratesR, 'r--')
         ax.plot(mg_indT, mg_ratesT, 'b--')
-    
+
     ax.set_xlabel(r"$\eta$")
     ax.set_ylabel("Key rate")
     #ax.legend(["No PS", "Transmitter PS", "Receiver PS", "Transmitter SC", "Mg no PS", "Mg r-PS", "Mg t-PS"])
-    
-    
+
+
     #key_rates1b = np.load(filename1 + "BAD.npy")
     #key_rates2b = np.load(filename2 + "BAD.npy")
     #key_rates3b = np.load(filename3 + "BAD.npy")
     #ax.plot(tes, key_rates1b, 'ko')
     #ax.plot(tes, key_rates2b, 'bo')
     #ax.plot(tes, key_rates3b, 'ro')
-    
+
     ax.set_yscale('log')
-    
-    
+
+
     locmaj = matplotlib.ticker.LogLocator(base=10,numticks=10)
     ax.yaxis.set_major_locator(locmaj)
     locmin = matplotlib.ticker.LogLocator(base=10.0,subs=(0.2,0.4,0.6,0.8),numticks=12)
     ax.yaxis.set_minor_locator(locmin)
     ax.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
-    
+
     #ax.minorticks_on()
     plt.legend()
     plt.show()
+
+
+
+def plot_split(N, params, MG=True):
+    i_stolens = []
+    i_shareds = []
+    indeces = []
+    options = ['none', 'tps', 'rps', 'tsc', 'rsc']
+#    options = ['none', 'tps', 'rps']
+
+    for option in options:
+
+        if option == 'rsc':
+            filename1 = names.measurements_line(10, 'I_shared', params, option)
+            filename2 = names.measurements_line(10, 'I_stolen', params, option)
+            filename_ind = names.indeces_line(10, 'I_s', params, option, 'eta')
+        else:
+            filename1 = names.measurements_line(N, 'I_shared', params, option)
+            filename2 = names.measurements_line(N, 'I_stolen', params, option)
+            filename_ind = names.indeces_line(N, 'I_s', params, option, 'eta')
+
+
+        i_shared = np.load(filename1 + ".npy")
+        i_shareds += [i_shared]
+
+        i_stolen = np.load(filename2 + ".npy")
+        i_stolens += [i_stolen]
+
+        inds = np.load(filename_ind + '.npy')
+        indeces += [inds]
+
+
+    fig1, ax = plt.subplots()
+    lines_types = ['k*-', 'b*-', 'r*-', 'y*-', 'm*-', 'c*-', 'g*-']
+    lines_types2 = ['ko-', 'bo-', 'ro-', 'yo-', 'mo-', 'co-', 'go-']
+
+    for i in range(len(i_shareds)):
+        ax.plot(indeces[i], i_shareds[i], lines_types[i], label=options[i])
+        ax.plot(indeces[i], i_stolens[i], lines_types2[i], label=options[i])
+
+    print(params)
+    ax.set_xlabel(r"$\eta$")
+    ax.set_ylabel("Key rate")
+    #ax.legend(["No PS", "Transmitter PS", "Receiver PS", "Transmitter SC", "Mg no PS", "Mg r-PS", "Mg t-PS"])
+
+
+    #key_rates1b = np.load(filename1 + "BAD.npy")
+    #key_rates2b = np.load(filename2 + "BAD.npy")
+    #key_rates3b = np.load(filename3 + "BAD.npy")
+    #ax.plot(tes, key_rates1b, 'ko')
+    #ax.plot(tes, key_rates2b, 'bo')
+    #ax.plot(tes, key_rates3b, 'ro')
+
+    ax.set_yscale('log')
+
+
+    locmaj = matplotlib.ticker.LogLocator(base=10,numticks=10)
+    ax.yaxis.set_major_locator(locmaj)
+    locmin = matplotlib.ticker.LogLocator(base=10.0,subs=(0.2,0.4,0.6,0.8),numticks=12)
+    ax.yaxis.set_minor_locator(locmin)
+    ax.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+
+    #ax.minorticks_on()
+    plt.legend()
+    plt.show()
+
+
+
 
 
 ## Parameters
@@ -105,21 +173,20 @@ def plot(N, params, MG=True):
 #plot(N, params)
 #
 #
-## Parameters
-#N = 20
-#mpn = 1.3
-#mpne = 0.001
-#f = 0.95
-#option = 'tsc'
-## Operations options
-#t = .1
-#k_ps = 0.5
-#k_sc = 0.1
-#k_ct = 0.5
-#
-##params = ["mpn=" + str(mpn), "mpne=" + str(mpne), "f=" + str(f) , "t=" + str(t)]
-#params = ["mpn=" + str(mpn), "mpne=" + str(mpne), "f=" + str(f) , "k_ps=" + str(k_ps),
-#          "k_sc=" + str(k_sc), "k_ct=" + str(k_ct)]
-#
-#plot(N, params)
+# Parameters
+N = 20
+mpn = 1.3
+mpne = 0.001
+f = 0.95
+option = 'tsc'
+# Operations options
+t = .1
+k_ps = 0.9
+k_sc = 0.1
+k_ct = 0.5
 
+#params = ["mpn=" + str(mpn), "mpne=" + str(mpne), "f=" + str(f) , "t=" + str(t)]
+params = ["mpn=" + str(mpn), "mpne=" + str(mpne), "f=" + str(f) , "k_ps=" + str(k_ps),
+         "k_sc=" + str(k_sc), "k_ct=" + str(k_ct)]
+
+plot_split(N, params)
--- src/measurements.py
+++ src/measurements.py
@@ -15,7 +15,7 @@ def key_rate_simple(sys, f, p):
     Vb = sys.get_simple_CM_V(1).norm()
     Ve = sys.get_simple_CM_V(2).norm()
     Vf = sys.get_simple_CM_V(3).norm()
-    
+
     Cab = sys.get_simple_CM_C([0, 1]).norm()
     Cbe = sys.get_simple_CM_C([1, 2]).norm()
     Cbf = sys.get_simple_CM_C([1, 3]).norm()
@@ -23,18 +23,18 @@ def key_rate_simple(sys, f, p):
 
     I_shared = I(Va, Vb, Cab)
     I_stolen = X(Vb, Ve, Vf, Cbe, Cbf, Cef)
-    
+
     k_rate = p * (f * I_shared - I_stolen)
     return k_rate
-    
+
 
 def key_rate(sys, f, p):
-    sys.set_quadratures_basis() 
+    sys.set_quadratures_basis()
     Va = sys.get_CM_entry([0, 0])
     Vb = sys.get_CM_entry([2, 2])
     Ve = sys.get_CM_entry([4, 4])
     Vf = sys.get_CM_entry([6, 6])
-    
+
     Cab = sys.get_CM_entry([0, 2])
     Cbe = sys.get_CM_entry([2, 4])
     Cbf = sys.get_CM_entry([2, 6])
@@ -42,18 +42,38 @@ def key_rate(sys, f, p):
 
     I_shared = I(Va, Vb, Cab)
     I_stolen = X(Vb, Ve, Vf, Cbe, Cbf, Cef)
-    
+
     k_rate = p * (f * I_shared - I_stolen)
     print("I_shared:", I_shared)
     print("I_stolen:", I_stolen)
     return k_rate
+
+
+def key_rate_split(sys, f, p):
+    sys.set_quadratures_basis()
+    Va = sys.get_CM_entry([0, 0])
+    Vb = sys.get_CM_entry([2, 2])
+    Ve = sys.get_CM_entry([4, 4])
+    Vf = sys.get_CM_entry([6, 6])
+
+    Cab = sys.get_CM_entry([0, 2])
+    Cbe = sys.get_CM_entry([2, 4])
+    Cbf = sys.get_CM_entry([2, 6])
+    Cef = sys.get_CM_entry([4, 6])
+
+    I_shared = I(Va, Vb, Cab)
+    I_stolen = X(Vb, Ve, Vf, Cbe, Cbf, Cef)
+
+    print("I_shared:", I_shared)
+    print("I_stolen:", I_stolen)
+    return I_shared, I_stolen
 
 def key_rate_compare(sys, f, p, mpnA, mpnE, t):
     Va = sys.get_simple_CM_V(0)
     Vb = sys.get_simple_CM_V(1)
     Ve = sys.get_simple_CM_V(2)
     Vf = sys.get_simple_CM_V(3)
-    
+
     Cab = sys.get_simple_CM_C([0, 1])
     Cbe = sys.get_simple_CM_C([2, 1])
     Cbf = sys.get_simple_CM_C([1, 3])
@@ -64,12 +84,12 @@ def key_rate_compare(sys, f, p, mpnA, mpnE, t):
     Vf2 = 2 * mpnE + 1
     Vb2 = t * Va2 + (1 - t) * Vf2
     Ve2 = (1 - t) * Va2 + t * Vf2
-    
+
     Cab2 = np.sqrt(t) * 2 * np.sqrt(mpnA**2 + mpnA)
     Cef2 = np.sqrt(t) * 2 * np.sqrt(mpnE**2 + mpnE)
-    Cbf2 = np.sqrt(1 - t)* 2 * np.sqrt(mpnE**2 + mpnE) 
-    Cbe2 = np.sqrt(t * (1 - t)) * (Vf2 - Va2) 
-    
+    Cbf2 = np.sqrt(1 - t)* 2 * np.sqrt(mpnE**2 + mpnE)
+    Cbe2 = np.sqrt(t * (1 - t)) * (Vf2 - Va2)
+
     # TPS
 #    N = sys.N
 #    Va2 = 1 + 2 * (N * mpnA + N + mpnA * t)/(1 + mpnA - mpnA * t)
@@ -82,12 +102,12 @@ def key_rate_compare(sys, f, p, mpnA, mpnE, t):
 #    Cbf2 = np.sqrt(1 - t) * 2 * np.sqrt(mpnE**2 + mpnE);
 #    Cbe2 = np.sqrt(t* (1 - t)) * (Vf2 - Vb2);
 
-    sys.set_quadratures_basis() 
+    sys.set_quadratures_basis()
     Va3 = sys.get_CM_entry([0, 0])
     Vb3 = sys.get_CM_entry([2, 2])
     Ve3 = sys.get_CM_entry([4, 4])
     Vf3 = sys.get_CM_entry([6, 6])
-    
+
     Cab3 = sys.get_CM_entry([0, 2])
     Cbe3 = sys.get_CM_entry([2, 4])
     Cbf3 = sys.get_CM_entry([2, 6])
@@ -98,7 +118,7 @@ def key_rate_compare(sys, f, p, mpnA, mpnE, t):
 #    Cef2 = abs(Cef2)
 #    Cbf2 = abs(Cbf2)
 #    Cbe2 = abs(Cbe2)
-    
+
     print("----------------t---------------", t)
     print("Parameter - Simple - Theory NOPS - Calculated")
     print("Va:", Va, Va2, Va3)
@@ -129,12 +149,12 @@ def key_rate_compare(sys, f, p, mpnA, mpnE, t):
 
 #    I_shared = I(Va, Vb, Cab)
 #    I_stolen = X(Vb, Ve, Vf, Cbe, Cbf, Cef)
-    
+
     k_rate = p * (f * I_shared - I_stolen)
-    
+
 #    print("I_shared:", f*I_shared)
 #    print("I_stolen:", I_stolen)
-    
+
     return k_rate
 
 def key_rate_theory(mpnA, mpnE, t):
@@ -142,67 +162,67 @@ def key_rate_theory(mpnA, mpnE, t):
     Vf = 2 * mpnE + 1
     Vb = t * Va + (1 - t) * Vf
     Ve = (1 - t) * Va + t * Vf
-    
+
     Cab = np.sqrt(t) * 2 * np.sqrt(mpnA**2 + mpnA)
     Cef = np.sqrt(t) * 2 * np.sqrt(mpnE**2 + mpnE)
     Cbf = np.sqrt(1 -t)* 2 * np.sqrt(mpnE**2 + mpnE)
     Cbe = np.sqrt(t * (1 - t)) * (Vf - Va)
-    
+
     I_shared = I(Va, Vb, Cab)
     I_stolen = X(Vb, Ve, Vf, Cbe, Cbf, Cef)
-    
+
     p = 1
     f = 1
-    k_rate = p * (f * I_shared - I_stolen) 
+    k_rate = p * (f * I_shared - I_stolen)
     return k_rate
 
 def I(Va, Vb, Cab):
-    # Conditional variance 
+    # Conditional variance
     Vb_a = Vb - (Cab**2)/Va
-    
+
     # Shared information
     si = np.log2(Vb/Vb_a)/2
     return si
-    
+
 def X(Vb, Ve, Vf, Cbe, Cbf, Cef):
     I = np.eye(2)
     S = np.diag([1, -1])
-    
+
     Mef = np.block([[I*Ve, S*Cef],[S*Cef, I*Vf]])
     vec = np.block([Cbe*I, Cbf*S])
     Mef_b = Mef - np.dot(vec.transpose(), np.dot(np.diag([1/Vb, 0]), vec))
-    
-    
+
+
     vef = symplectic_eigenvalues(Mef)
 #    vef2 = .5 * np.sqrt((Ve + Vf)**2 - 4 * Cef**2) + .5 * np.array([Ve-Vf, Vf -Ve])
 #    print("Vef:", vef, vef2)
-    
+
     vef_b = symplectic_eigenvalues(Mef_b)
-    
+
     # Calculate the stolen information
     si = np.sum(g(vef)) - np.sum(g(vef_b))
     return si
-    
- 
+
+
 def log_neg(rho, pos_transp):
     if rho.isket:
         rho = rho * rho.dag()
-    
+
     # State with partial transpose
     rho = qt.partial_transpose(rho, pos_transp)
 
     norm = rho.norm()
     return np.log2(norm)
-       
+
 
 def log_neg_Gauss(sys, pos_transp):
     if sys.state.isket:
         rho = sys.state * sys.state.dag()
     else:
         rho = sys.state
-        
+
     rho = qt.partial_transpose(rho, pos_transp)
-    sys.set_state(rho) 
+    sys.set_state(rho)
     sys.set_quadratures_basis()
     cm = sys.get_full_CM()
     sym_eig = symplectic_eigenvalues(cm)
@@ -211,9 +231,9 @@ def log_neg_Gauss(sys, pos_transp):
     return log_neg
 
 def g(v):
-    return ((v+1)/2)*(np.log2((v+1)/2)) - ((v-1)/2)*(safe_log2((v-1)/2))    
-    
-    
+    return ((v+1)/2)*(np.log2((v+1)/2)) - ((v-1)/2)*(safe_log2((v-1)/2))
+
+
 def safe_log2(x, minval=0.0000000001):
     return np.log2(x.clip(min=minval))
 
@@ -258,16 +278,16 @@ def CI(sys, pos_trace_out):
     # Get both covariance matrices
     sys.set_quadratures_basis()
     CM1 = sys.get_full_CM()
-    
+
     sys.ptrace(pos_trace_out)
     sys.set_quadratures_basis()
     CM2 = sys.get_full_CM()
-    
+
 #    vef1 = np.max(symplectic_eigenvalues(CM1))
 #    vef2 = np.max(symplectic_eigenvalues(CM2))
     vef1 = symplectic_eigenvalues(CM1)
     vef2 = symplectic_eigenvalues(CM2)
-    
+
     ci = np.sum(g(vef2)) - np.sum(g(vef1))
 #    ci = sum(g(vef2)) - sum(g(vef1))
     return ci
