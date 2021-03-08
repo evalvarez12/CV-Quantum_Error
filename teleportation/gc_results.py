2cab7adaa05353ab5b74da64ae14ab5519ad3d0c
Eduardo Villasenor <evalvarez12@gmail.com>
2021-03-08 15:48:27 +1100

    Getting some results with qs

---
+++ teleportation/gc_results.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Create results for the GLOBECOMM paper
+Created on Mon Mar  8 11:32:18 2021
+
+@author: z5239621
+"""
+
+
+import TMSV as tmsv
+import qsc as qs_t
+import qsc_rec as qs_r
+import scipy.io
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+############################# FIXED LOSS FIDELITIES PLOT
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+T = np.linspace(0.01, 1)
+eps = 0.02
+eta = 10**(-1/10)
+sigmaT = [2, 4, 10, 25]
+sigmaD = [2, 4, 10, 25]
+
+f_tmsv = []
+f_qs_t = []
+f_qs_r = []
+
+for sig_ind in range(len(sigmaT)):
+    f_tmsv_i = []
+    f_qs_t_i = []
+    f_qs_r_i = []
+    
+    for it in T:
+        f_tmsv_i += [tmsv.opt_fidelity_alphabet_gopt(it, eps, eta, sigmaT[sig_ind])]
+        f_qs_t_i += [qs_t.opt_avg_fidelity(it, eps, eta, sigmaT[sig_ind])]
+        f_qs_r_i += [qs_r.opt_avg_fidelity(it, eps, eta, sigmaT[sig_ind])]
+    
+    f_tmsv += [f_tmsv_i]
+    f_qs_t += [f_qs_t_i]
+    f_qs_r += [f_qs_r_i]
+
+    
+    
+    
+ax.plot(T, f_tmsv[0], label=r'TMSV $\sigma= $' + str(np.round(np.abs(sigmaT[0]),2)), linestyle='-', marker='o', markersize=4, linewidth=1.5)
+ax.plot(T, f_tmsv[1], label=r'TMSV $\sigma= $' + str(np.round(np.abs(sigmaT[1]),2)), linestyle='-', marker='o', markersize=4, linewidth=1.5)
+ax.plot(T, f_tmsv[2], label=r'TMSV $\sigma= $' + str(np.round(np.abs(sigmaT[2]),2)), linestyle='-', marker='o', markersize=4, linewidth=1.5)
+ax.plot(T, f_tmsv[3], label=r'TMSV $\sigma= $' + str(np.round(np.abs(sigmaT[3]),2)), linestyle='-', marker='o', markersize=4, linewidth=1.5)
+
+ax.plot(T, f_qs_t[0], label=r'QS_t $\sigma= $' + str(np.round(np.abs(sigmaD[0]),2)), linestyle='-', marker='*', markersize=4, linewidth=1.5)
+ax.plot(T, f_qs_t[1], label=r'QS_t $\sigma= $' + str(np.round(np.abs(sigmaD[1]),2)), linestyle='-', marker='*', markersize=4, linewidth=1.5)
+ax.plot(T, f_qs_t[2], label=r'QS_t $\sigma= $' + str(np.round(np.abs(sigmaD[2]),2)), linestyle='-', marker='*', markersize=4, linewidth=1.5)
+ax.plot(T, f_qs_t[3], label=r'QS_t $\sigma= $' + str(np.round(np.abs(sigmaD[3]),2)), linestyle='-', marker='*', markersize=4, linewidth=1.5)
+
+ax.plot(T, f_qs_r[0], label=r'QS_r $\sigma= $' + str(np.round(np.abs(sigmaD[0]),2)), linestyle='-', marker='*', markersize=4, linewidth=1.5)
+ax.plot(T, f_qs_r[1], label=r'QS_r $\sigma= $' + str(np.round(np.abs(sigmaD[1]),2)), linestyle='-', marker='*', markersize=4, linewidth=1.5)
+ax.plot(T, f_qs_r[2], label=r'QS_r $\sigma= $' + str(np.round(np.abs(sigmaD[2]),2)), linestyle='-', marker='*', markersize=4, linewidth=1.5)
+ax.plot(T, f_qs_r[3], label=r'QS_r $\sigma= $' + str(np.round(np.abs(sigmaD[3]),2)), linestyle='-', marker='*', markersize=4, linewidth=1.5)
+
+
+clasical = np.ones_like(f_tmsv[0]) * .5
+plt.plot(T, clasical, 'r--')
+
+ax.set_xlabel(r'Fixed Loss')
+ax.set_ylabel(r'$\bar{\mathcal{F}} $')
+# plt.ylim([-0.005, 0.2])
+
+#ax.set_xlim(-0.1, 13)
+#ax.set_ylim(.49, 1)
+#ax.set_xscale('log')
+ax.grid()
+ax.legend()
+
+### To MATLAB
+#data = [Tdb, f_tele1, f_tele2, f_tele3, f_tele4, f_dir1, f_dir2, f_dir3, f_dir4]
+#scipy.io.savemat('matlab/fixed.mat', {'data':data})
--- teleportation/qsc_rec.py
+++ teleportation/qsc_rec.py
@@ -8,53 +8,83 @@ Created on Mon Feb  1 10:18:53 2021
 import numpy as np
 import scipy.optimize as op
 import matplotlib.pyplot as plt
+import TMSV as tmsv
+
+
+
+def avg_fidelity(V, T, tsc, eps, eta, g, sigma):
+    gsc = np.sqrt((1 - tsc)/tsc)
+    g = g*eta
+    gt = g - 1
+
+#    print('gsc:', gsc)
+#    print(np.round([V, tsc, g], 3))
+
+    A = 0.5 * (T * (V - 1) + eps + 2)
+    B = np.sqrt(T * (V**2 - 1))
+    A2 = 0.5 * (V - (T * (V**2 - 1))/(T * (T - 1) + eps + 2))
+    A3 = 1/A + gsc**2 * (1/A - 1/A**2)
+    B1u = - gsc * B/(2*A**2) - B**2/4*A**3 - g**2 * (1/A - 1/A**2)
+    B1v = gsc * B/(2*A**2) - B**2/4*A**3 - g**2 * (1/A - 1/A**2)
+    A4 = g**2 * B**2 / (4 * A**3)
+    A5 = A2 + 2 * g**2 + 1 - (g/eta)**2 * (1 - eta**2)
+    A6 = A3/A5 + A4/A5**3 * (12 + 1/4) + 1/(2 * A5**2) * (B1u + B1v)
+    B2u = - B1u * gt**2 / A5**3 - (A4 / A5**4) * gt**2 * (24 + 1/2)
+    B2v = - B1v * gt**2 / A5**3 - (A4 / A5**4) * gt**2 * (24 + 1/2)
+    A7 = (A4 / A5**5) * gt**4
+    A8 = gt**2/A5 + 1/sigma
+    
+    norm = 1/A - gsc**2 / ((1 + gsc**2) * A**2)  
+    
+#    print('A1:', A1)
+#    print('A2:', A2)
+#    print('A3:', A3)
+#    print('A5:', A5)
+#    print('A6:', A6)
+#    print('norm:', norm)
+
+    F = (1 / (1 + gsc**2)) * 1/sigma * (A6/A8 + (1/(2*A8**2))* (B2u + B2v) + A7/(A8**3)* (96 + 1/4))
+    return F
+    
 
 def fidelity(V, T, tsc, eps, eta, g, alpha):
-    print('pars opt:', np.round(V, 3), np.round(tsc, 3), np.round(g, 3))
+#    print('pars opt:', np.round(V, 3), np.round(tsc, 3), np.round(g, 3))
 
     gsc = np.sqrt((1 - tsc)/tsc)
     g = g*eta
     gt = g - 1
 
-#    print(gsc, gt)
-
-    A = 1/2 * (T*(V-1) + eps + 2)
-    A2 = 1/ ((1 + gsc**2) *A)
-    A3 = T * (V**2 - 1)/ (4*A)
-    B1 = (gsc / (2 * A)) * np.sqrt(T * (V**2 - 1))
-    A4 = 1/2 * (V + 2*A3 + g**2)
-    A5 = 1 - gsc**2 * (1 - 1/A)
-    A6 = -B1*g + gsc**2 * (A3/A - g * (1 - 1/A))
-    A7 = (gsc * g)**2 * A3/A
-    A8 = g**2/2 + A4 + g**2*(1-eta**2)/2
-    A9 = A8 + 1/2
-    A10 = gt**2/A9
+    print('gsc:', gsc)
+    print(np.round([V, tsc, g], 3))
 
 
-    # print('A:', A)
-    # print('A2:', A2)
-    # print('A3:', A3)
-    # print('B1:', B1)
-    # print('A4:', A4)
-    # print('A5:', A5)
-    # print('A6:', A6)
-    # print('A7:', A7)
-    # print('A8:', A8)
-    # print('A9:', A9)
-    # print('A10:', A10)
+    A = 0.5 * (T * (V - 1) + eps + 2)
+    B = np.sqrt(T * (V**2 - 1))
+    A2 = 0.5 * (V - (T * (V**2 - 1))/(T * (T - 1) + eps + 2))
+    A3 = 1/A + gsc**2 * (1/A - 1/A**2)
+    B1u = - gsc * B/(2*A**2) - B**2/4*A**3 - g**2 * (1/A - 1/A**2)
+    B1v = gsc * B/(2*A**2) - B**2/4*A**3 - g**2 * (1/A - 1/A**2)
+    A4 = g**2 * B**2 / (4 * A**3)
+    A5 = A2 + 2 * g**2 + 1 - (g/eta)**2 * (1 - eta**2)
+    A6 = A3/A5 + A4/A5**3 * (12 + 1/4) + 1/(2 * A5**2) * (B1u + B1v)
+    B2u = - B1u * gt**2 / A5**3 - (A4 / A5**4) * gt**2 * (24 + 1/2)
+    B2v = - B1v * gt**2 / A5**3 - (A4 / A5**4) * gt**2 * (24 + 1/2)
+    A7 = (A4 / A5**5) * gt**4
+    
+    norm = 1/A - gsc**2 / ((1 + gsc**2) * A**2)
 
-    F = A2*np.exp(-A10 * np.abs(alpha)**2) * (A5/A9 + ((A6/(np.sqrt(2)*A9**3)) \
-        * (4*A9 - 4*gt**2*np.abs(alpha)**2)) + ((A7/(2*A9**5)) \
-        * (28*A9**2 - 56*A9*(gt*np.abs(alpha))**2 + 16*gt**4*(np.real(alpha)**4) \
-           + np.imag(alpha)**4 + (np.real(alpha)*np.imag(alpha))**2)))
+    
+#    print('A1:', A1)
+#    print('A2:', A2)
+#    print('A3:', A3)
+#    print('A4:', A4)
+#    print('A5:', A5)
+    print('norm:', norm)
 
-
-
-    F2 = A2 * np.exp(-A10*np.abs(alpha)**2) * (A5/A9 + (A6*2*np.sqrt(2))/(A9**2) + 14*A7/(A9**3) \
-                      - (np.abs(alpha)**2 * ((2*np.sqrt(2)*g**2*A6)/A9**3 + (28*g**2*A7)/A9**4)) \
-                      + 8*g**4*A7/A9**5 * (np.real(alpha)*np.imag(alpha))**2 \
-                      + 8*g**4*A7/A9**5 * (np.real(alpha)**4 + np.imag(alpha)**4))
-
+    F = (1 / (1 + gsc**2)) * np.exp(-(gt**2/A5)*np.abs(alpha)**2) * \
+        (A6 + B2u * np.imag(alpha)**2 + B2v * np.real(alpha)**2 + \
+         A7 * (np.real(alpha)*np.imag(alpha))**2 + \
+         8*A7 * (np.real(alpha)**4 + np.imag(alpha)**4))
     return F
 
 
@@ -62,18 +92,31 @@ def fidelity_pars(pars, T, eps, eta, alpha):
     V, tsc, g = pars
     return fidelity(V, T, tsc, eps, eta, g, alpha)
 
+
+def avg_fidelity_pars(pars, T, eps, eta, sigma):
+    V, tsc, g = pars
+    return avg_fidelity(V, T, tsc, eps, eta, g, sigma)
+
+
+
+
+def get_cons():
+    cons=({'type': 'ineq',
+       'fun': lambda x: x[0] - 1.001},
+      {'type': 'ineq',
+       'fun': lambda x: 10000 - x[0]},
+      {'type': 'ineq',
+       'fun': lambda x: x[1] - 0.001},
+      {'type': 'ineq',
+       'fun': lambda x: .99 - x[1]},
+      {'type': 'ineq',
+       'fun': lambda x: x[2]})
+    return cons
+
 def opt_fidelity(T, eps, eta, alpha):
     F = lambda P : 1 - fidelity_pars(P, T, eps, eta, alpha)
     initial_guess = [2, .2, 1]
-    cons=({'type': 'ineq',
-           'fun': lambda x: x[0] - 1},
-          {'type': 'ineq',
-           'fun': lambda x: x[1]},
-          {'type': 'ineq',
-           'fun': lambda x: 1.1-x[1]},
-          {'type': 'ineq',
-           'fun': lambda x: x[2]})
-    
+    cons= get_cons()
 
     res = op.minimize(F, initial_guess, constraints=cons)
 #    res = op.minimize(F, initial_guess)
@@ -82,19 +125,54 @@ def opt_fidelity(T, eps, eta, alpha):
     return fidelity_pars(res['x'], T, eps, eta, alpha)
 
 
+def opt_avg_fidelity(T, eps, eta, sigma):
+    F = lambda P : 1 - avg_fidelity_pars(P, T, eps, eta, sigma)
+    initial_guess = [1.5, .2, 1]
+    cons= get_cons()
+
+    res = op.minimize(F, initial_guess, constraints=cons)
+#    res = op.minimize(F, initial_guess)
+    print(res)
+#    print('opt V:', np.round(res['x'],3))
+    return avg_fidelity_pars(res['x'], T, eps, eta, sigma)
+
 ################################################
-V = np.random.rand()*4 + 1
-T = np.random.rand()
-tsc = np.random.rand() * 0.5
-tsc = 0.2
-T = .005
-V = 1.8
-eps = 0
-alpha = 1
+V = np.random.rand() + 1
+T = np.random.rand() * 1
+tsc = np.random.rand() 
+#T = 1
+#V = 1.00001
+eps = 0.05
+alpha = 5
+sigma = 5
+eta = 1
+g = 1
+print('-------------Random')
+print('pars:', np.round([V, T, tsc], 3))
+print('F:', fidelity(V, T, tsc, eps, eta, g, alpha))
+print('F_avg:', avg_fidelity(V, T, tsc, eps, eta, g, sigma))
+print('-------------------')
+#print('F_opt', opt_fidelity(T, eps, 1, alpha))
 
-print('pars:', V, T, tsc)
-print('F:', fidelity(V, T, tsc, eps, 1, 1, alpha))
+eps = 0.05
+alpha = 5
 
-print('F_opt', opt_fidelity(T, eps, 1, alpha))
+sigma = 2
+eta = 1
+g = 1
+T = 0.01
+tsc = 0.04
+V = 1.4
+alpha = 10
+gsc = np.sqrt((1 - tsc)/tsc)
+#print('gsc:', gsc)
+#print('p_succ:', 2 / (V+1) - (gsc*2/(V+1))**2 * (1 / (1 + gsc**2)))
+#
+#print('F:', fidelity(V, T, tsc, eps, eta, g, alpha))
+#print('F_opt', opt_fidelity(T, eps, eta, sigma))
+print('F_TMSV:', tmsv.fidelity(V, T, eps, eta, g, alpha))
+print('F_avg:', avg_fidelity(V, T, .01, eps, eta, g, sigma))
 
+print('F_avg_TMSV:', tmsv.opt_fidelity_alphabet_gopt(T, eps, eta, sigma))
+print('F_avg_opt', opt_avg_fidelity(T, eps, eta, sigma))
 #print('F hand:', fidelity_pars([1.6, .999, .541], T, eps, 1, alpha))
