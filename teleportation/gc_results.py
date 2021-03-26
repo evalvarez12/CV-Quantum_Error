Fixed factor of 2 on sb, all good now

teleportation/gc_results.py
@@ -93,6 +93,7 @@ eps = 0.005
 eta = np.sqrt(10**(-1/10))
 sigma = [2, 5, 10, 20]
 # eta = 1
+eta = 0.7
 
 f_tmsv = []
 f_qs_t = []
@@ -107,7 +108,7 @@ for sig_ind in range(len(sigma)):
         f_tmsv_i += [tmsv.opt_avg_fidelity(it, eps, eta, sigma[sig_ind])]
         f_qs_t_i += [qs_t.opt_avg_fidelity(it, eps, eta, sigma[sig_ind])]
 #        f_qs_r_i += [qs_r.opt_avg_fidelity(it, eps, eta, sigmaT[sig_ind])]
-        f_sb_i += [sb.opt_avg_fidelity(it, eps, np.sqrt(eta), sigma[sig_ind])]
+        f_sb_i += [sb.opt_avg_fidelity(it, eps, eta, sigma[sig_ind])]
 
 
     f_tmsv += [f_tmsv_i]
@@ -127,10 +128,10 @@ ax.plot(T, f_tmsv[3], label=r'TMSV $\sigma= $' + str(np.round(np.abs(sigma[3]),2
 #ax.plot(T, f_qs_t[2], label=r'QS_t $\sigma= $' + str(np.round(np.abs(sigma[2]),2)), linestyle='-', marker='*', markersize=4, linewidth=1.5)
 #ax.plot(T, f_qs_t[3], label=r'QS_t $\sigma= $' + str(np.round(np.abs(sigma[3]),2)), linestyle='-', marker='*', markersize=4, linewidth=1.5)
 
-ax.plot(T, f_sb[0], label=r'QS_r $\sigma= $' + str(np.round(np.abs(sigma[0]),2)), linestyle='-', marker='v', markersize=4, linewidth=1.5)
-ax.plot(T, f_sb[1], label=r'QS_r $\sigma= $' + str(np.round(np.abs(sigma[1]),2)), linestyle='-', marker='v', markersize=4, linewidth=1.5)
-ax.plot(T, f_sb[2], label=r'QS_r $\sigma= $' + str(np.round(np.abs(sigma[2]),2)), linestyle='-', marker='v', markersize=4, linewidth=1.5)
-ax.plot(T, f_sb[3], label=r'QS_r $\sigma= $' + str(np.round(np.abs(sigma[3]),2)), linestyle='-', marker='v', markersize=4, linewidth=1.5)
+ax.plot(T, f_sb[0], label=r'SB_r $\sigma= $' + str(np.round(np.abs(sigma[0]),2)), linestyle='-', marker='v', markersize=4, linewidth=1.5)
+ax.plot(T, f_sb[1], label=r'SB_r $\sigma= $' + str(np.round(np.abs(sigma[1]),2)), linestyle='-', marker='v', markersize=4, linewidth=1.5)
+ax.plot(T, f_sb[2], label=r'SB_r $\sigma= $' + str(np.round(np.abs(sigma[2]),2)), linestyle='-', marker='v', markersize=4, linewidth=1.5)
+ax.plot(T, f_sb[3], label=r'SB_r $\sigma= $' + str(np.round(np.abs(sigma[3]),2)), linestyle='-', marker='v', markersize=4, linewidth=1.5)
 
 
 clasical = np.ones_like(f_tmsv[0]) * .5
@@ -148,6 +149,6 @@ ax.legend()
 #ax.set_xscale('log')
 ### To MATLAB
 data = [T, f_tmsv, f_qs_t, f_sb]
-scipy.io.savemat('figs_noG/fixed_py.mat', {'data':data})
+scipy.io.savemat('figs_noG/fixed_py_eta0.7.mat', {'data':data})
 
 plt.show()

teleportation/sb.py
@@ -140,6 +140,7 @@ def Delta(r, t, g, T, nth):
     return res
 
 def Gamma(r, t, g, T, nth):
-    R = (1 - T**2) 
+    # NOTE: this fix of /2 to R
+    R = (1 - T**2)/2 
     res = (1 - np.exp(-t)) * (.5 + nth) + g**2 * R
     return res

teleportation/test_fidelities.py
@@ -35,14 +35,16 @@ if True:
     # V = np.linspace(1, 150, 80)
     #T = np.logspace(-3, 0, 15)
     R = np.array([0, 0.05, 0.1, 0.15])
-    T = (1 - R)
-#    T = [1, 0, 0.81, 0.9]
+#    T = (1 - R)
+    T = [1, 0.9, 0.8, 0.7]
     eps = 0.00
     eta = np.sqrt(10**(-1/10))
     alpha = 15
     # eta = 1
     f_tmsv = []
     f_sb = []
+
+    eta = 0.9
 
     g = 1
     for iT in T:
@@ -52,12 +54,12 @@ if True:
         for iV in V:
            iV = np.cosh(2*iV)
            f_tmsv_i += [tmsv.opt_fidelity_r(iV, iT, eps, eta, alpha)]
-           f_sb_i += [sb.opt_fidelity_r(iV, iT, eps, np.sqrt(eta), alpha)]
+           f_sb_i += [sb.opt_fidelity_r(iV, iT, eps, eta, alpha)]
 #           f_tmsv_i += [tmsv.opt_avg_fidelity_r(iV, iT, eps, eta, alpha)]
 #           f_sb_i += [sb.opt_avg_fidelity_r(iV, iT, eps, np.sqrt(eta), alpha)]
 #
 #           f_tmsv_i += [tmsv.opt_avg_fidelity_r(iV, 1, eps, iT, alpha)]
-#           f_sb_i += [sb.opt_avg_fidelity_r(iV, 1, eps, iT, alpha)]
+#           f_sb_i += [sb.opt_avg_fidelity_r(iV, 1, eps, np.sqrt(iT), alpha)]
 
 #           f_tmsv_i += [tmsv.opt_fidelity_r(iV, 1, eps, iT, alpha)]
 #           f_sb_i += [sb.opt_fidelity_r(iV, 1, eps, np.sqrt(iT), alpha)]