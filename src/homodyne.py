e9330bd29ff5de9edbef9559be87d0c96a80597d
Eduardo Villasenor <evalvarez12@gmail.com>
2021-09-11 15:57:24 +1000

    Something wrong with Homodyne projector

--- src/homodyne.py
+++ src/homodyne.py
@@ -7,24 +7,40 @@ import numpy as np
 import tools
 
 # OLD function save just in case
-# def get_homodyne_projector(self, x_measured):
-#     a = qt.create(self.N)
-#     x = (np.sqrt(2) * x_measured * a) - (x_measured**2 + a*a)/2
-#     x = 1/np.pi**(1/4) * x.expm()
-#     x = (x * qt.basis(self.N)).dag()
-#     return x
 
 
-def get_homodyne_projector(N, q_m, theta, eta):
+def get_homodyne_projector_OLD(N, x_measured):
+    a = qt.create(N)
+    x = (np.sqrt(2) * x_measured * a) - (x_measured**2 + a*a)/2
+    x = 1/np.pi**(1/4) * x.expm()
+    # x = (x * qt.basis(N)).dag()
+    return x
+
+
+def pdf_projector(N, q_m, theta, eta):
     a = qt.destroy(N)
 
     # Using SNU units
-    q = a.dag() + a
-    p = 1j*(a.dag() - a)
+    q = (a.dag() + a)/np.sqrt(2)
+    p = 1j*(a.dag() - a)/np.sqrt(2)
     qtheta = q*np.cos(theta) + p*np.sin(theta)
     # Projector obtained from : 10.1103/RevModPhys.81.299
     P = (eta * (-(q_m/eta)**2 + 2*q_m*qtheta/eta - qtheta**2)).expm()
     return P/np.sqrt(np.pi/eta)
+
+
+def pdf_projector_red_dim(N, q_m, theta, eta):
+    a = qt.destroy(N)
+
+    # Using SNU units
+    q = (a.dag() + a)/np.sqrt(2)
+    p = 1j*(a.dag() - a)/np.sqrt(2)
+    qtheta = q*np.cos(theta) + p*np.sin(theta)
+    # Projector obtained from : 10.1103/RevModPhys.81.299
+    P = (eta * (-(q_m/eta)**2 + 2*q_m*qtheta/eta - qtheta**2)).expm()
+    P = P/np.sqrt(np.pi/eta)
+    P = (P * qt.basis(N)).dag()
+    return P
 
 
 def homodyne_measurement(state, mode=0, theta=0, eta=1, mlim=[-5, 5], sample_size=20):
@@ -34,9 +50,12 @@ def homodyne_measurement(state, mode=0, theta=0, eta=1, mlim=[-5, 5], sample_siz
     q_samples = np.linspace(mlim[0], mlim[1], sample_size)
     samples = np.zeros_like(q_samples)
     for i in range(sample_size):
-        projector = get_homodyne_projector(N, q_samples[i], theta, eta)
+        projector = pdf_projector(N, q_samples[i], theta, eta)
         projector = tools.tensor(N, projector, mode, Nmodes)
         samples[i] = qt.expect(projector, state)
+
+    # Check the area of the PDF
+    print("SUM PDF:", np.sum(samples * (q_samples[1] - q_samples[0])))
 
     # Perform 1D polyfit to approximate the PDF
     fit = np.polyfit(q_samples, samples, 20)
@@ -44,30 +63,32 @@ def homodyne_measurement(state, mode=0, theta=0, eta=1, mlim=[-5, 5], sample_siz
     # New points to find pdf
     qs = np.linspace(mlim[0], mlim[1], 10 * sample_size)
     ds = np.abs(qs[0] - qs[1])
-    pdf = pdf_func(qs) * ds
-    pdf[pdf < 0] = 0
-    pdf = pdf / np.sum(pdf)
+    pdf = pdf_func(qs)
 
     # import matplotlib.pyplot as plt
     # plt.plot(q_samples, samples)
     # plt.plot(qs, pdf)
     # plt.show()
-    # from IPython import embed
-    # embed()
+    # # from IPython import embed
+    # # embed()
+
+    pdf = pdf * ds
+    pdf[pdf < 0] = 0
+    pdf = pdf / np.sum(pdf)
 
     # Select a measurement result
     q_measured = np.random.choice(qs, p=pdf)
     # q_measured = np.random.choice(
     #     q_samples, p=samples/sum(samples))
-    projector = get_homodyne_projector(N, q_measured, theta, eta)
+    projector = pdf_projector(N, q_measured, theta, eta)
     projector = tools.tensor(N, projector, mode, Nmodes)
 
     if state.isket:
         state = state * state.dag()
 
-    state = projector.dag() * state * projector
     prob = (state * projector).tr()
-    state = state / prob
+    state = projector.dag() * state * projector
+    # state = state / prob
     return state, prob
 
 
@@ -87,7 +108,7 @@ def homodyne_measurement_gauss_approx(state, mode=0, theta=0, eta=1):
 
     # Select random measurment result
     q_measured = np.random.normal(avg_q, np.sqrt(var_q))
-    projector = get_homodyne_projector(N, q_measured, theta, eta)
+    projector = pdf_projector(N, q_measured, theta, eta)
     projector = tools.tensor(N, projector, mode, Nmodes)
 
     if state.isket:
--- src/test_homodyne.py
+++ src/test_homodyne.py
@@ -1,10 +1,14 @@
 import cv_system as cv
 import wigner_plots as wp
-N = 20
-a = 1j
+import matplotlib.pyplot as plt
+import homodyne as hom
+import qutip as qt
 
-sys = cv.System(N, 1)
-sys.apply_SMD(a)
+N = 30
+a = 3
+
+sys = cv.System(N, 2)
+sys.apply_TMS(a)
 
 # wp.plot(sys.state, [-5, 5])
 
@@ -12,5 +16,25 @@ sys.apply_SMD(a)
 # sys.apply_photon_addition(t, 1, 1)
 
 state, prob = sys.homodyne_measurement()
+
+
+P1 = hom.pdf_projector(N, q_m=1, theta=0, eta=1)
+P2 = hom.get_homodyne_projector_OLD(N, x_measured=1)
+P3 = qt.displace(N, 1) * qt.squeeze(N, 15) * qt.basis(N)
+P3 = P3 * P3.dag()
+
+P1 = qt.tensor(P1, qt.qeye(N))
+P3 = qt.tensor(P3, qt.qeye(N))
+
+# wp.plot(P, [-5, 5])
+# wp.plot(P2, [-5, 5])
+# wp.plot(P3, [-5, 5])
+
+s1 = P1 * sys.state
+s3 = P3 * sys.state
+
+wp.plot(sys.state.ptrace(0), [-5, 5])
+wp.plot(s1.ptrace(0), [-5, 5])
+wp.plot(s3.ptrace(0), [-5, 5])
 
 # wp.plot(state, [-5, 5])
