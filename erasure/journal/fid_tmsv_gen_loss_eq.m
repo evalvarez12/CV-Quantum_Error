5c8384cc8cc5e8f623ddf451818f638c8d40b344
Eduardo Villasenor <evalvarez12@gmail.com>
2022-10-04 16:24:33 +1100

    Added hom measurement loss

--- erasure/journal/fid_tmsv_gen_loss_eq.m
+++ erasure/journal/fid_tmsv_gen_loss_eq.m
@@ -1,4 +1,7 @@
-function [F] = fid_tmsv_gen_loss_eq(V, g, sigma, T1r, T2r, T3r, eps)
+function [F] = fid_tmsv_gen_loss_eq(V, g, sigma, T1r, T2r, T3r, eps, nu)
+
+
+gp = g*nu;
 
 T1 = sqrt(T1r);
 T2 = sqrt(T2r);
@@ -13,16 +16,17 @@ T2p = sqrt(1 + eps - T2r);
 T3p = sqrt(1 + eps - T3r);
 
 
-a = 1/4*(V*((T12 + g.*T21).^2 + (T3.*g).^2)) - V2/2*(T12 + g.*T21).*T3.*g;
-b = 1/4*(T21 + g.*T12).^2;
-c = 1/4*(T1p.^2/2 + T2p.^2/2 + g.*(T1p.^2 - T2p.^2) + g.^2.*(T1p.^2/2 + T2p.^2/2 + T3p.^2));
-d = sqrt(2)*(T21 + g.*T12);
+a = (V*((T12 + gp.*T21).^2 + (T3.*gp).^2)) - 2*V2*(T12 + gp.*T21).*T3.*gp;
+b = (T21 + gp.*T12).^2;
+c = 1 + g.^2*(1+nu^2) + eps*(1+2.*gp.^2) - T1^2/2.*(1+gp).^2 - T2^2/2.*(1-gp).^2 - T3^2.*gp.^2;
 
-a2 = a + b + c + 1/4;
+d = sqrt(2)*(T21 + gp.*T12);
+
+a2 = a + b + c + 1;
 b2 = d - sqrt(2);
 
 % F =  4 ./ (b2.^2*sigma + 4*a2);
-F =  2 ./ (b2.^2*sigma + 4*a2);
+F =  2 ./ (b2.^2*sigma + a2);
 
 % F = real(F);
 end
